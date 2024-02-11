15367af9841802ca9b21f79c780e97ea736d22c6
Petra Smejkalova <smejkalova.petra@gmail.com>
2024-02-11 19:17:12 +0100

    first commit

---
+++ mesta/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { cities } from './data/cz-cities';
+import City from './components/city/City';
+
+const App = () => (
+  <>
+    <h1>Cities</h1>
+    <ul className="city-list">
+      {cities.map((city) => (
+        <City
+          key={city.name}
+          name={city.name}
+          population={city.population}
+          area={city.area}
+          district={city.district}
+        />
+      ))}
+    </ul>
+  </>
+);
+
+export default App;
---
+++ mesta/src/components/city/City.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const City = ({ name, population, area, district }) => (
+  <div className="city">
+    <div className="city__info">
+      <li className="city__name">{name}</li>
+      <li className="city__population">Population: {population}</li>
+      <li className="city__area">Area: {area}</li>
+      <li className="city__district">District: {district}</li>
+    </div>
+    <img className="city__photo" src="{photo}" />
+  </div>
+);
+
+export default City;
