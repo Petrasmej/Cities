0203f290948bc4540d332b9290478b9fca2e554e
Petra Smejkalova <smejkalova.petra@gmail.com>
2024-02-14 12:40:24 +0100

    added photos

--- mesta/src/components/city/City.jsx
+++ mesta/src/components/city/City.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const City = ({ name, population, area, district }) => (
+const City = ({ name, population, area, district, photo }) => (
   <div className="city">
     <div className="city__info">
       <li className="city__name">{name}</li>
@@ -8,7 +8,7 @@ const City = ({ name, population, area, district }) => (
       <li className="city__area">Area: {area}</li>
       <li className="city__district">District: {district}</li>
     </div>
-    <img className="city__photo" src="{photo}" />
+    <img className="city__photo" src={photo} />
   </div>
 );
 
